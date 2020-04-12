b3a8d00e07c41f02b97ab8ae86b0f2e4bb8e001a
David Mendoza Martínez <david.mendoza.mnez@gmail.com>
2020-04-13 01:20:08 +0200

    Add AppGlideModule in commons module

--- commons/src/main/java/com/davidmendozamartinez/gangame/commons/BindingAdapter.kt
+++ commons/src/main/java/com/davidmendozamartinez/gangame/commons/BindingAdapter.kt
@@ -2,11 +2,10 @@ package com.davidmendozamartinez.gangame.commons
 
 import android.widget.ImageView
 import androidx.databinding.BindingAdapter
-import com.bumptech.glide.Glide
 
 @BindingAdapter("imageUrl")
 fun loadImage(imageView: ImageView, url: String) {
-    Glide.with(imageView)
+    GlideApp.with(imageView)
         .load(url)
         .into(imageView)
 }
